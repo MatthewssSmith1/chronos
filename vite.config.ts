88efe06f490e08b8ea2e1bead4c3ea2556710a97
Matt Smith <matthewsssmith1@gmail.com>
2025-02-22 12:00:16 -0600

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite({}), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
+})
